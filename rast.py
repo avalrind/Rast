a0b2d9d235c53b3fbb64386e8e74be8c143f68c6
avalrind <142172024+avalrind@users.noreply.github.com>
2024-01-09 08:26:00 +0530

    Create rast.py

---
+++ rast.py
@@ -0,0 +1,50 @@
+class rast : 
+
+    class Include : 
+
+        def __init__(self, name , value) : 
+            self.name = name
+            self.value = value
+
+    class BuiltInFunction : 
+
+        def __init__(self, name, func) : 
+            
+            self.name = name
+            self.func = func
+
+    class NamedVariable : 
+
+        def __init__(self, name , typer , value) : 
+            
+            self.name = name
+            self.typer = typer
+            self.value = value
+
+    class Filename : 
+
+        def __init__(self, name , body) : 
+
+            self.name = name
+            self.body = body
+
+    class FunctionDef : 
+
+        def __init__(self , name , body , args) : 
+
+            self.name = name
+            self.body = body
+            self.args = args
+
+    class Return : 
+
+        def __init__(self , value) : 
+
+            self.value = value
+
+    class SRO : # Scope Resolution Operator
+            
+            def __init__(self , name , body) : 
+    
+                self.name = name
+                self.body = body
